de1bcb0f0831404253f161d0418754d6e1814f95
虞程龙 <80099403+770690988@users.noreply.github.com>
2022-01-02 10:56:38 +0800

    85分新增

--- assets/js/main.js
+++ assets/js/main.js
@@ -70,7 +70,7 @@ function nextDay(m) { //m表示股票的个数
 
     }
     document.getElementById('dayNum').innerText = dayNum;
-    if (dayNum >=5) {
+    if (dayNum >=200) {
         jump(zichanreload(shareNum));
     } else if (dayNum % 10 == 0) {
         var str = "答题时间，回答正确会获得相应奖励\n\n" + sharesExercise[dayNum / 10 - 1] + "\n\n正确选确定 错误选取消";
